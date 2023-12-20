2f506f7eed571000982584b189d4bda1318f2d35
Diego Pereyra <diego.pereyra99@gmail.com>
2023-12-20 02:55:23 +0100

    Included different architectures to train the model

--- train_model.py
+++ train_model.py
@@ -1,28 +1,28 @@
 import argparse
 import os
 from keras.preprocessing.image import ImageDataGenerator
-from keras.models import Sequential, Model
+from keras.models import Model
 from keras.layers import Dense, Dropout
 from keras.callbacks import ModelCheckpoint
 from keras.applications import MobileNetV2, ResNet50, EfficientNetV2B3, InceptionV3
 import pandas as pd
 
+model_archs = {
+    "mobilenet": MobileNetV2,
+    "resnet": ResNet50,
+    "inception": InceptionV3,
+    "efficientnet": EfficientNetV2B3,
+}
 
 def load_backbone(arch="mobilenet"):
     
-    base_model = MobileNetV2(
+    ModelClass = model_archs[arch]
+    base_model = ModelClass(
         include_top=False,
         weights="imagenet", 
         input_shape=(224,224,3),
         pooling="avg"
     )
-
-    # base_model = InceptionV3(
-    #     include_top=False,
-    #     weights="imagenet", 
-    #     input_shape=(224,224,3),
-    #     pooling="avg"
-    # )
     
     return base_model
 
@@ -30,7 +30,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Train a model with TensorFlow.')
     parser.add_argument('--epochs', type=int, default=5, help='Number of training epochs')
     parser.add_argument('--output-name', type=str, default='model.h5', help='Output name for saved model')
-    parser.add_argument("--architecture", type=str, default="mobilenetv2", help="Architecture of the model to be trained")
+    parser.add_argument("--architecture", type=str, default="mobilenet", help="Architecture of the model to be trained", choices=list(model_archs.keys()))
     parser.add_argument("--train-backbone", action="store_true", help="Include this option to train the weights of the backbone")
 
     args = parser.parse_args()
